788149d59be5e0ae1e0e28fb839ebc71d66697cf
Guy Shalev <guy.shalev.email@gmail.com>
2024-11-15 10:22:40 +0200

    Initial commit

---
+++ src/test/java/com/guyshalev/Salt_security/SaltSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyshalev.Salt_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaltSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
